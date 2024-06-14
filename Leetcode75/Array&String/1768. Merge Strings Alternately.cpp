03ff85582c976791cb835af1616cad0fb55fe8f4
YU <0928half@gmail.com>
2024-06-15 07:49:47 +0800

    Update 1768. Merge Strings Alternately.cpp

--- Leetcode75/Array&String/1768. Merge Strings Alternately.cpp
+++ Leetcode75/Array&String/1768. Merge Strings Alternately.cpp
@@ -1,4 +1,19 @@
 /*
+You are given two strings word1 and word2. Merge the strings by adding letters in alternating order, starting with word1. 
+If a string is longer than the other, append the additional letters onto the end of the merged string.
+
+Return the merged string.
+Example 1:
+
+Input: word1 = "abc", word2 = "pqr"
+Output: "apbqcr"
+Explanation: The merged string will be merged as so:
+word1:  a   b   c
+word2:    p   q   r
+merged: a p b q c r
+*/
+
+/*
 solution1:
 -Time complexity: O(n**2)
 外層for迴圈執行n次
